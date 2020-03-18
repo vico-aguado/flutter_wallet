649f997eedd7730da005d4349836a5e8c9c00ccd
Víctor Aguado <vico_aguado@hotmail.com>
2020-03-18 15:56:42 -0600

    Fix with FlutterStandardTypedData error

--- ios/Classes/FlutterWalletPlugin.m
+++ ios/Classes/FlutterWalletPlugin.m
@@ -14,10 +14,38 @@
   if ([@"addWalletPass" isEqualToString:call.method]) {
       
       if (call.arguments != nil) {
-        
           if ([call.arguments[@"pkpass"] isEqual:[NSNull null]]) {
               result([FlutterError errorWithCode:@"WITHOUT_PARAMETERS" message:@"Don't have 'pkpass' parameter" details:@"You need add 'pkpass' parameter"]);
           } else {
+              //NSString *className = NSStringFromClass([call.arguments[@"pkpass"] class]);
+              //NSLog(@"call.arguments['pkpass'] is a: %@", className);
+              
+              if ([call.arguments[@"pkpass"] isKindOfClass:[FlutterStandardTypedData class]]) {
+                FlutterStandardTypedData *pkpassdata = call.arguments[@"pkpass"];
+                  
+                NSError* errorPass = nil;
+                PKPass *newPass = [[PKPass alloc] initWithData:pkpassdata.data
+                                                         error:&errorPass];
+                
+                if (errorPass!=nil) {
+                    result([FlutterError errorWithCode:@"PKPASS_ERROR" message:[errorPass localizedDescription] details:nil]);
+                }else{
+                    UIViewController *rootController = [UIApplication sharedApplication].delegate.window.rootViewController;
+                    
+                    if (rootController != nil) {
+                        PKAddPassesViewController *addController = [[PKAddPassesViewController alloc] initWithPass:newPass];
+                        [rootController presentViewController:addController animated:YES completion:nil];
+                    } else {
+                        result([FlutterError errorWithCode:@"ROOTCONTROLLER_INVALID" message:@"rootViewController of the app is invalid" details:nil]);
+                    }
+                    result([NSNumber numberWithBool:YES]);
+                }
+              } else {
+                result([FlutterError errorWithCode:@"PARAMETERS_INVALID" message:@"Your 'pkpass' parameter is invalid (Not is FlutterStandardTypedData)" details:nil]);
+              }
+              
+              
+              /*
               if ([call.arguments[@"pkpass"] isKindOfClass:[NSArray class]]) {
                   NSArray *pkpassArray = call.arguments[@"pkpass"];
                   if ([pkpassArray count] > 0) {
@@ -45,11 +73,11 @@
                           result([NSNumber numberWithBool:YES]);
                       }
                   } else {
-                      result([FlutterError errorWithCode:@"PARAMETERS_INVALID" message:@"Your 'pkpass' parameter is invalid (Empty)" details:nil]);
+                      result([FlutterError errorWithCode:@"PARAMETERS_INVALID" message:@"Your 'pkpass' parameter is invalid" details:nil]);
                   }
               } else {
-                  result([FlutterError errorWithCode:@"PARAMETERS_INVALID" message:@"Your 'pkpass' parameter is invalid (NSArray)" details:nil]);
-              }
+                  result([FlutterError errorWithCode:@"PARAMETERS_INVALID" message:@"Your 'pkpass' parameter is invalid" details:nil]);
+              }*/
           }
       }else{
           result([FlutterError errorWithCode:@"WITHOUT_PARAMETERS" message:@"Don't have 'pkpass' parameter" details:@"You need add 'pkpass' parameter"]);
